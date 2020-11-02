e8add193d519802580fd552e0907637de124df69
Akond Rahman <akond.rahman.buet@gmail.com>
2020-11-02 12:33:04 -0600

    Category-9: Detect absence of Logging handled

--- FAME-ML/main.py
+++ FAME-ML/main.py
@@ -9,5 +9,5 @@ import lint_engine
 if __name__=='__main__':
     TEST_ML_SCRIPT = '../../Datasets/sample.test.py' 
     data_load_count = lint_engine.getDataLoadCount( TEST_ML_SCRIPT ) 
-    print(data_load_count) 
+    
 
--- FAME-ML/py_parser.py
+++ FAME-ML/py_parser.py
@@ -10,6 +10,39 @@ import os
 import constants 
 
 
+def checkLogging(tree_object, list_func_defs, name2track):
+    LOGGING_EXISTS_FLAG = False 
+    IMPORT_FLAG, FUNC_FLAG, ARG_FLAG  = False, False , False 
+    for stmt_ in tree_object.body:
+        for node_ in ast.walk(stmt_):
+            if isinstance(node_, ast.Import) :
+                funcDict = node_.__dict__     
+                # print(funcDict) 
+                import_name_objects = funcDict[constants.NAMES_KW]
+                for obj in import_name_objects:
+                    if ( constants.LOGGING_KW in  obj.__dict__[constants.NAME_KW]): 
+                        IMPORT_FLAG = True 
+    func_decl_list = getPythonAtrributeFuncs(tree_object)
+    for func_decl_ in func_decl_list:
+        func_parent_id, func_name , funcLineNo, call_arg_list = func_decl_ # the class in which the method belongs, func_name, line no, arg_list 
+        
+        if ( constants.LOGGING_KW in func_parent_id ) or ( constants.LOGGING_KW in func_name) : 
+            # print(func_parent_id, func_name, call_arg_list)  
+            FUNC_FLAG = True 
+            for arg_ in call_arg_list:
+                if name2track in arg_:
+                    ARG_FLAG = True 
+    if (IMPORT_FLAG) and (FUNC_FLAG) and (ARG_FLAG):
+        LOGGING_EXISTS_FLAG = True 
+    return LOGGING_EXISTS_FLAG 
+    
+
+
+
+
+  
+
+
 def getPythonParseObject( pyFile ): 
     full_tree = ast.parse( open( pyFile  ).read() )    
     return full_tree 
@@ -36,5 +69,7 @@ def getPythonAtrributeFuncs(pyTree):
                                 arg_dic  = funcArg.__dict__
                                 arg_name = arg_dic[constants.ATTRIB_KW] 
                                 call_arg_list.append( (  arg_name, constants.INDEX_KW + str(x_ + 1) )  ) 
+                            elif(isinstance( funcArg, ast.Str ) ):
+                                call_arg_list.append( ( funcArg.s, constants.INDEX_KW + str( x_ + 1 )  ) )
                         attrib_call_list.append( ( func_parent.id, func_name , funcLineNo, call_arg_list  ) )                
     return attrib_call_list 
