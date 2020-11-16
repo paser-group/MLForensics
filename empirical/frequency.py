972d3c0a542fb515fccb233d23172c172a10187d
Akond Rahman <akond.rahman.buet@gmail.com>
2020-11-16 12:17:56 -0600

    code for rq2

--- empirical/frequency.py
+++ empirical/frequency.py
@@ -15,6 +15,13 @@ def giveTimeStamp():
   return strToret
 
 
+def getAllSLOC(df_param, csv_encoding='latin-1' ):
+    total_sloc = 0
+    all_files = np.unique( df_param['FILE_FULL_PATH'].tolist() ) 
+    for file_ in all_files:
+        total_sloc = total_sloc + sum(1 for line in open(file_, encoding=csv_encoding))
+    return total_sloc
+
 def reportProportion( res_file ):
     res_df = pd.read_csv( res_file )
     all_py_files   = np.unique( res_df['FILE_FULL_PATH'].tolist() )
@@ -32,6 +39,20 @@ def reportProportion( res_file ):
         # print(field_df.head())  
 
 
+def reportEventDensity(output_file): 
+  output_df = pd.read_csv(output_file) 
+  all_py_files   = np.unique( output_df['FILE_FULL_PATH'].tolist() )  
+  all_py_size    = getAllSLOC(output_df) 
+  
+  fields2explore = ['DATA_LOAD_COUNT', 'MODEL_LOAD_COUNT', 'DATA_DOWNLOAD_COUNT',	'MODEL_LABEL_COUNT', 'MODEL_OUTPUT_COUNT',	
+                      'DATA_PIPELINE_COUNT', 'ENVIRONMENT_COUNT', 'STATE_OBSERVE_COUNT', 'DNN_DECISION_COUNT', 'TOTAL_EVENT_COUNT'
+                     ]
+  for field in fields2explore:
+    field_res_list  = output_df[field].tolist() 
+    field_res_count = sum( field_res_list ) 
+    event_density   = round( float(field_res_count * 1000 ) / float(all_py_size)  , 5) 
+    print('TOTAL_LOC:{}, CATEGORY:{}, TOTAL_EVENT_COUNT:{}, EVENT_DENSITY:{}'.format( all_py_size, field, field_res_count, event_density )  )
+    print('-'*25)
 
 if __name__=='__main__': 
     print('*'*100 )
@@ -47,6 +68,8 @@ if __name__=='__main__':
     RESULTS_FILE = '/Users/arahman/Documents/OneDriveWingUp/OneDrive-TennesseeTechUniversity/Research/VulnStrategyMining/ForensicsinML/Output/V3_OUTPUT_MODELZOO.csv'    
     
     reportProportion( RESULTS_FILE )
+    print('*'*100) 
+    reportEventDensity(  RESULTS_FILE )
     
     print('*'*100 )
     print('Ended at:', giveTimeStamp() )
